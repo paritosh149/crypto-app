40e16bad28d6d011c2541dbf3eb750041063b0fd
Paritosh Mishra <paritosh149@outlook.com>
2019-11-10 22:51:12 +1100

    Error Handling UI applied

--- src/App.js
+++ src/App.js
@@ -1,58 +1,99 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import { connect } from 'react-redux'
-import { fetchItems } from './actions/actions'
+import { connect } from "react-redux";
+import { fetchItems, closeError } from "./actions/actions";
 
-import Container from '@material-ui/core/Container'
-import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
-import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Snackbar from "@material-ui/core/Snackbar";
+import Slide from "@material-ui/core/Slide";
 
-import ProfitDisplayBox from './components/ProfitDisplayBox'
+import ProfitDisplayBox from "./components/ProfitDisplayBox";
 
-import ItemType from './PropTypes/Item.proptype'
+import ItemType from "./PropTypes/Item.proptype";
 
 export class App extends React.Component {
-    render() {
-        return (
-            <Container maxWidth="xs">
-                <Box my={4}>
-                    <Typography variant="h6" component="h4" gutterBottom>
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  render() {
+    return (
+      <Container maxWidth="xs">
+        <Box my={4}>
+          <Typography variant="h6" component="h4" gutterBottom>
             Crypto Best Profit Finder
-                    </Typography>
-                    <Button variant="contained" color="primary" disabled={this.props.isFetching} onClick={this.props.fetchItems} >
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            disabled={this.props.isFetching}
+            onClick={this.props.fetchItems}
+          >
             Fetch Data
-                    </Button>
-                    <br /> <br />
-                    <Grid container spacing={3}>
-                        {this.props.items && this.props.items.map(item => <ProfitDisplayBox key={item.currency} item={item} />)}
-                    </Grid>
-                </Box>
-            </Container>
-        )
-    }
+          </Button>
+          <br /> <br />
+          <Grid container spacing={3}>
+            {this.props.items.length > 0 &&
+              this.props.items.map(item => (
+                <ProfitDisplayBox key={item.currency} item={item} />
+              ))}
+            {this.props.items.length === 0 &&
+              this.props.isError &&
+              this.props.isOpen && (
+                <Snackbar
+                  open={true}
+                  onClose={this.props.closeError}
+                  TransitionComponent={props => (
+                    <Slide {...props} direction="up" />
+                  )}
+                  ContentProps={{
+                    "aria-describedby": "message-id"
+                  }}
+                  message={
+                    <span id="message-id">
+                      Error:{this.props.error.message}
+                    </span>
+                  }
+                />
+              )}
+          </Grid>
+        </Box>
+      </Container>
+    );
+  }
 }
 
 App.propTypes = {
-    isFetching: PropTypes.bool,
-    fetchItems: PropTypes.func,
-    items: PropTypes.arrayOf(ItemType)
-}
+  isFetching: PropTypes.bool,
+  fetchItems: PropTypes.func,
+  items: PropTypes.arrayOf(ItemType),
+  isOpen: PropTypes.bool,
+  isError: PropTypes.bool
+};
 
 function mapStateToProps(state) {
-    const { isFetching, items } = state || {
-        isFetching: true,
-        items: []
-    }
-    return { isFetching, items }
+  const { isFetching, items, isOpen, error, isError } = state || {
+    isFetching: true,
+    items: [],
+    isOpen: false,
+    isError: false
+  };
+  return { isFetching, items, isOpen, error, isError };
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        fetchItems: event => dispatch(fetchItems())
-    }
-}
+  return {
+    fetchItems: event => dispatch(fetchItems()),
+    closeError: () => dispatch(closeError())
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,30 +1,49 @@
-export const REQUEST_DATA = 'REQUEST_DATA'
-export const RECEIVE_DATA = 'RECEIVE_DATA'
+export const REQUEST_DATA = "REQUEST_DATA";
+export const RECEIVE_DATA = "RECEIVE_DATA";
+export const RECEIVE_ERROR = "RECEIVE_ERROR";
+export const CLOSE_ERROR = "CLOSE_ERROR";
 
 function requestData() {
-    return {
-        type: REQUEST_DATA
-    }
+  return {
+    type: REQUEST_DATA
+  };
 }
 
 function receiveData(json) {
-    return {
-        type: RECEIVE_DATA,
-        items: json
-    }
+  return {
+    type: RECEIVE_DATA,
+    items: json
+  };
+}
+
+function receiveError(error) {
+  return {
+    type: RECEIVE_ERROR,
+    error: error
+  };
+}
+
+export function closeError() {
+  return {
+    type: CLOSE_ERROR
+  };
 }
 
 function fetchData() {
-    return dispatch => {
-        dispatch(requestData())
-        return fetch('http://localhost:3030/data/best')
-            .then(response => response.json())
-            .then(json => dispatch(receiveData(json)))
-    }
+  return dispatch => {
+    dispatch(requestData());
+    return fetch("http://localhost:3030/data/best")
+      .then(response => response.json())
+      .then(json => dispatch(receiveData(json)))
+      .catch(error => {
+        console.error("Fetch failed", error);
+        dispatch(receiveError(error));
+      });
+  };
 }
 
-export function fetchItems(){
-    return (dispatch, getState) => {
-        return dispatch(fetchData())
-    }
+export function fetchItems() {
+  return (dispatch, getState) => {
+    return dispatch(fetchData());
+  };
 }
