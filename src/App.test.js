6c33b787dcb5c27178150c47a07ddd45fa54243f
Paritosh Mishra <paritosh149@outlook.com>
2019-11-11 00:43:02 +1100

    Test Coverage applied

--- src/App.test.js
+++ src/App.test.js
@@ -7,37 +7,37 @@ import Button from '@material-ui/core/Button'
 import ProfitDisplayBox from './components/ProfitDisplayBox'
 
 describe('App Component', () => {
-  let props
-  let mount
+    let props
+    let mount
 
-  beforeAll(() => {
-    mount = createMount()
-    props = {
-      items: [],
-      isFetching: false
-    }
-  })
+    beforeAll(() => {
+        mount = createMount()
+        props = {
+            items: [],
+            isFetching: false
+        }
+    })
 
-  it('renders without crashing', () => {
-    const tree = renderer.create(<App {...props} />)
+    it('renders without crashing', () => {
+        const tree = renderer.create(<App {...props} />)
 
-    expect(tree.toJSON()).toMatchSnapshot()
-  })
+        expect(tree.toJSON()).toMatchSnapshot()
+    })
 
-  it('calls the fetchData function when the button is clicked', () => {
-    props.fetchItems = jest.fn()
-    const wrapper = mount(<App {...props} />)
-    const spy = jest.spyOn(wrapper.instance().props, 'fetchItems')
+    it('calls the fetchData function when the button is clicked', () => {
+        props.fetchItems = jest.fn()
+        const wrapper = mount(<App {...props} />)
+        const spy = jest.spyOn(wrapper.instance().props, 'fetchItems')
 
-    wrapper.find(Button).simulate('click')
-    expect(spy).toHaveBeenCalled()
-  })
+        wrapper.find(Button).simulate('click')
+        expect(spy).toHaveBeenCalled()
+    })
 
-  it('renders the ProfitDisplayBox component correctly', () => {
-    props.items = [{ "currency": "BTC", "date": "2018-05-07T00:00:00.000+10:00", "best": { "buy": { "time": "2019-11-10T09:15:00.000+11:00", "price": "34.98" }, "sell": { "time": "2019-11-10T12:30:00.000+11:00", "price": "37.01" }, "profit": 2.03 } }]
-    const wrapper = mount(<App {...props} />);
+    it('renders the ProfitDisplayBox component correctly', () => {
+        props.items = [{ 'currency': 'BTC', 'date': '2018-05-07T00:00:00.000+10:00', 'best': { 'buy': { 'time': '2019-11-10T09:15:00.000+11:00', 'price': '34.98' }, 'sell': { 'time': '2019-11-10T12:30:00.000+11:00', 'price': '37.01' }, 'profit': 2.03 } }]
+        const wrapper = mount(<App {...props} />)
 
-    expect(wrapper.find(ProfitDisplayBox).length).toBe(1);
-  });
+        expect(wrapper.find(ProfitDisplayBox).length).toBe(1)
+    })
 
 })
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,36 +1,36 @@
 import {
-  REQUEST_DATA,
-  RECEIVE_DATA,
-  RECEIVE_ERROR,
-  CLOSE_ERROR
-} from "../actions/actions";
+    REQUEST_DATA,
+    RECEIVE_DATA,
+    RECEIVE_ERROR,
+    CLOSE_ERROR
+} from '../actions/actions'
 
 function itemReducer(
-  state = {
-    isFetching: false,
-    items: [],
-    isOpen: false
-  },
-  action
-) {
-  switch (action.type) {
-    case REQUEST_DATA:
-      return { ...state, isFetching: true };
-    case RECEIVE_DATA:
-      return { ...state, isFetching: false, items: action.items };
-    case RECEIVE_ERROR:
-      return {
-        ...state,
+    state = {
         isFetching: false,
-        isError: true,
-        error: action.error,
-        isOpen: true
-      };
+        items: [],
+        isOpen: false
+    },
+    action
+) {
+    switch (action.type) {
+    case REQUEST_DATA:
+        return { ...state, isFetching: true }
+    case RECEIVE_DATA:
+        return { ...state, isFetching: false, items: action.items }
+    case RECEIVE_ERROR:
+        return {
+            ...state,
+            isFetching: false,
+            isError: true,
+            error: action.error,
+            isOpen: true
+        }
     case CLOSE_ERROR:
-      return { ...state, isOpen: false };
+        return { ...state, isOpen: false }
     default:
-      return state;
-  }
+        return state
+    }
 }
 
-export default itemReducer;
+export default itemReducer
